74f85ab3480450f618cd78c4a7e3807cf9c219e3
KinTamashii <77307334+KinTamashii@users.noreply.github.com>
2023-06-09 20:45:39 -0400

    Delete test directory after adding to .gitignore

--- test/debug/ml5/decode_clean.sh
+++
@@ -1,7 +0,0 @@
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/../../util.sh"
-
-
-$msm_exec -verbose \
-    `callcmd decode clean $citra_dump_dir/0004000000132700/Msg/US_en $SCRIPT_DIR/clean "widths=\"$citra_dump_dir/0004000000132700/Msg/US_en/font.bffnt,100,-1\"" "archive_bg4 file"`
